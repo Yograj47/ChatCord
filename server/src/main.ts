b38b337dc242aadbdb8fae660825821101828808
Yograj Rijal <yograjrijal926@gmail.com>
2026-09-05 17:48:53 +0545

    feat(server): complete backend foundation

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,8 +2,9 @@ import dns from 'node:dns';
 
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
@@ -19,8 +20,28 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = configService.getOrThrow<number>('app.port');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
+  const config = new DocumentBuilder()
+    .setTitle('ChatCord API')
+    .setDescription(
+      'REST API for the ChatCord real-time communication platform',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    useGlobalPrefix: true,
+  });
+  const port = configService.getOrThrow<number>('app.port');
   await app.listen(port);
 
   console.log(`ChatCord API running on http://localhost:${port}/api`);
